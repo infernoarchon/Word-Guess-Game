0cc05a9816da5f6fd4cc8efb57b9cec008b44a1a
Chen <achen@c02vmakphtd5.hbo.homebox.com>
2018-10-11 17:34:53 -0400

    game mechanics v1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+
+
+// Start the Game
 var gamestarted = false
 document.onkeyup = function(event) {
     if(gamestarted === false) {
@@ -9,15 +12,44 @@ document.onkeyup = function(event) {
     }
 }
 
+// Answers
+
+var composers = {
+    "mozart": {
+        "name":"mozart",
+        "song":"../sounds/somesonata.mp3"
+    },
+    "debussy": {
+        "name":"debussy",
+        "song":"../sounds/clairdelune.mp3"
+    },
+    "beethoven": {
+        "name":"beethoven",
+        "song":"../sounds/moonlight.mp3"
+    }
+}
+
+console.log(Object.keys(composers).length)
+
+// console.log(mozart.name.length) to get artist name length
+
+// Game Mechanics
 var hangman = {
     start() {
         gamestarted = true;
         document.getElementById("gameui").setAttribute("class","visible");
+        hangman.generateword()
     },
-    
+    generateword() {
+        var random = Math.floor((Math.random() * Object.keys(composers).length));
+        var currentComposer = Object.keys(composers)[random]
+        console.log(currentComposer)
+    }
 }
 
-var mozart = {
-
-}
-
+// - Generate blank spaces according to composer name length and play music
+// - Record letter press, only call next function if letter status is either incorrect or correct
+// - Loop through each letter to check if it's correct
+// - If correct, assign boolean and print letter in blank space
+// - If incorrect, assign boolean and print letter on bottom
+// - If correct guesses counter === compoaser name length, stop music, refresh guess counter and load next word

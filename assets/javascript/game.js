693063d3f155b9614a2c5cf7c7307ebf9ca65c81
Chen <achen@c02vmakphtd5.hbo.homebox.com>
2018-10-11 15:03:34 -0400

    startscreen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ document.onkeyup = function(event) {
     if(gamestarted === false) {
         hangman.start();
         document.getElementById("intro").setAttribute("class","hidden");
+        document.getElementById("introtitle").setAttribute("class","hidden");
+        document.getElementById("banner").innerHTML = '<video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src="assets/images/concertoloop.mp4" type="video/mp4"></video>';
         $('.transform').toggleClass('transform-active');
     }
 }

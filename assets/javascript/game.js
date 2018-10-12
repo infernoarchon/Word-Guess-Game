ac623e476bbb547e8cbcaf37a8a9707f9f14996f
Ali C <17682708+infernoarchon@users.noreply.github.com>
2018-10-12 05:34:11 -0400

    added skip button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,19 +9,30 @@
             $('.transform').toggleClass('transform-active');
         }
     }
-
-    // Answers
-
-
-    // console.log(mozart.name.length) to get artist name length
+    document.getElementById("skip").onclick = function() {
+        hangman.cleanup();
+        hangman.getword();
+    }
 
     // Game Mechanics
+    var globalaudio = "";
+    var globalcomposer = "";
     var hangman = {
         // Start Game
         start : function() {
             gamestarted = true;
             document.getElementById("gameui").setAttribute("class","visible");
             hangman.getword();
+        },
+        cleanup : function() {
+            globalaudio.pause()
+            // Get the <ul> element with id="myList"
+            var list = document.getElementById("word");
+
+            // As long as <ul> has a child node, remove it
+            while (list.hasChildNodes()) {   
+                list.removeChild(list.firstChild);
+            }
         },
         getword : function() {
             var getrandom = function() {
@@ -39,31 +50,39 @@
                     var trackurl = composers[x].song
                     var audio = new Audio(trackurl);
                     audio.play();
+                    globalaudio = audio
                 }
                 createspace(currentComposer);
                 playTrack(currentComposer);
+                globalcomposer = currentComposer;
             }
             // Add composers Here
-            var composers = {
-                "mozart": {
-                    "song":"./assets/sounds/sonatak331.mp3"
-                    },
-                "debussy": {
-                    "song":"./assets/sounds/clairdelune.mp3"
-                    },
-                "beethoven": {
-                    "song":"./assets/sounds/moonlight.mp3"
-                    },
-                "tchaikovsky": {
-                    "song":"./assets/sounds/october.mp3"
-                    },
-                "chopin": {
-                    "song":"./assets/sounds/balladesop23.mp3"
-                    },              
-            }
             getrandom();
+            delete composers[globalcomposer]
+            console.log(composers)
         }
     }
+    var composers = {
+        "mozart": {
+            "song":"./assets/sounds/sonatak331.mp3"
+            },
+        "debussy": {
+            "song":"./assets/sounds/clairdelune.mp3"
+            },
+        "beethoven": {
+            "song":"./assets/sounds/moonlight.mp3"
+            },
+        "tchaikovsky": {
+            "song":"./assets/sounds/october.mp3"
+            },
+        "chopin": {
+            "song":"./assets/sounds/balladesop23.mp3"
+            },              
+    }
+ // Skip Word
+//  document.getElementById("skip").onclick = function() {
+//     hangman.getword()
+// }
 // - Generate blank spaces according to composer name length and play music
 // - Record letter press, only call next function if letter status is either incorrect or correct
 // - Loop through each letter to check if it's correct
